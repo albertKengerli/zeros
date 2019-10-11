8d9324ad2bb48ee05bb62b94491ca77376beb1c5
albertKengerli <50487338+albertKengerli@users.noreply.github.com>
2019-10-11 17:45:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let counterTwo = 0;
+  let counterFive = 0;
+  let expressionArray = [];
+  let baseArray = [];
+  let degreeArray = [];
+
+  function countMultiplicity(number, multiplier) {
+    let counter = 0;
+    while (number % multiplier == 0) {
+      counter++;
+      number /= multiplier;
+    }
+    return counter;
+  }
+
+  function factorial (base, degree) {
+    if (base <= 0) base = 1;
+    counterTwo += countMultiplicity(base,2);
+    counterFive += countMultiplicity(base,5);
+    return (base != 1) ? base * factorial(base-degree, degree) : base;
+  }
+
+  expressionArray = expression.split("*");
+
+  expressionArray.forEach(element => {
+    degreeArray.push((element.match(/!/g)).length);
+    element = element.replace(/!/g, "");
+    baseArray.push(element);
+  })
+
+  baseArray.forEach((element, i) => {
+    factorial(element, degreeArray[i]);
+  })
+
+  return counterFive > counterTwo ? counterTwo: counterFive;
 }
